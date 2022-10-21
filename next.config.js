a8293729d7a03c075e3a3229cb0c1f07e19fec5f
Vitor Rezende <dev.rezendevitor@gmail.com>
2022-10-21 14:29:55 -0300

    git workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const withPWA = require('next-pwa')
+/** @type {import('next').NextConfig} */
 
-module.exports = withPWA({
-    pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public'
-    }
-})
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
+
+module.exports = nextConfig;
